7d485258c2f19e452c0b5631995c9970ba53fea2
野声 <lijiacheng.ljc@antgroup.com>
2022-10-12 17:22:36 +0800

    feat: update version info

--- src/notifyRCResult.ts
+++ src/notifyRCResult.ts
@@ -5,7 +5,7 @@ call(async ({ github, context, core }) => {
     owner: context.repo.owner,
     repo: context.repo.repo,
     body:
-      `<!-- versionInfo: RC | ${process.env.CURRENT_VERSION} -->\n` +
+      `<!-- versionInfo: Release Candidate | ${process.env.CURRENT_VERSION} -->\n` +
       `Release Candidate Summary:\n\n${process.env.SUMMARY}`,
     commit_sha: process.env.CURRENT_COMMIT,
   });
