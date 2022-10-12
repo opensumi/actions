f465886f30622a931bd5659fcef35739cccffaf5
野声 <lijiacheng.ljc@antgroup.com>
2022-10-12 14:29:17 +0800

    fix: fix build

--- src/notifyRCResult.ts
+++ src/notifyRCResult.ts
@@ -2,8 +2,8 @@ import { call } from './core';
 
 call(async ({ github, context, core }) => {
   await github.rest.repos.createCommitComment({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
+    owner: 'opensumi',
+    repo: 'core',
     body: `Summary:\n${process.env.GITHUB_STEP_SUMMARY}`,
     commit_sha: process.env.CURRENT_COMMIT,
   });
