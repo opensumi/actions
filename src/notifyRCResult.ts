84a7047909d9164c19efc6ece160a12271dd9a13
野声 <lijiacheng.ljc@antgroup.com>
2022-10-12 14:23:21 +0800

    fix: fix rc notify

--- src/notifyRCResult.ts
+++ src/notifyRCResult.ts
@@ -1,13 +1,10 @@
 import { call } from './core';
-import { createVersionText } from './helpers';
 
 call(async ({ github, context, core }) => {
-  const commentBody = createVersionText('Release Candidate', process.env.CURRENT_VERSION, context);
-
   await github.rest.repos.createCommitComment({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: commentBody,
+    body: process.env.GITHUB_STEP_SUMMARY,
     commit_sha: process.env.CURRENT_COMMIT,
   });
 });
