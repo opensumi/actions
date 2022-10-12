a3b02fad0ed20716aa276bf954553c7cb0ddc637
野声 <lijiacheng.ljc@antgroup.com>
2022-10-12 14:55:35 +0800

    feat: update notify result

--- src/notifyRCResult.ts
+++ src/notifyRCResult.ts
@@ -4,7 +4,9 @@ call(async ({ github, context, core }) => {
   await github.rest.repos.createCommitComment({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: `Release Candidate Summary:\n\n${process.env.SUMMARY}`,
+    body:
+      `<!-- versionInfo: RC | ${process.env.CURRENT_VERSION} -->\n` +
+      `Release Candidate Summary:\n\n${process.env.SUMMARY}`,
     commit_sha: process.env.CURRENT_COMMIT,
   });
 });
