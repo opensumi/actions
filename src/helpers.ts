2605db509628e93f3a0c7daa60ea260ca9472869
野声 <lijiacheng.ljc@antgroup.com>
2022-10-12 14:17:53 +0800

    feat: notify add step summary

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,24 +1,29 @@
 import { Context } from './core';
 
-export const createVersionText = (type = 'Pre-Release', version: string, context: Context) => {
+export const createVersionText = (
+  type = 'Pre-Release',
+  version: string,
+  context: Context
+) => {
   if (!context.payload.comment) {
     return '';
   }
   return (
     `🎉 ${type} version ` +
-    version +
-    ' publish successful! You can install prerelease version via `npm install package@' +
-    version +
-    ' `' +
-    ' [@' +
-    context.payload.comment.user.login +
-    ']' +
-    '(https://github.com/' +
-    context.payload.comment.user.login +
-    ')\n\n' +
-    '```\n' +
-    version +
-    '\n' +
-    '```\n'
+      version +
+      ' publish successful! You can install prerelease version via `npm install package@' +
+      version +
+      ' `' +
+      ' [@' +
+      context.payload.comment.user.login +
+      ']' +
+      '(https://github.com/' +
+      context.payload.comment.user.login +
+      ')\n\n' +
+      '```\n' +
+      version +
+      '\n' +
+      '```\n' +
+      process.env.GITHUB_STEP_SUMMARY ?? ''
   );
 };
