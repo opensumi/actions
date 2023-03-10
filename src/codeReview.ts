8c2613e77b3841c663e79e9fdc4f874ddb9c53e5
野声 <lijiacheng.ljc@antgroup.com>
2023-03-10 15:35:58 +0800

    chore: update dist

--- src/codeReview.ts
+++ src/codeReview.ts
@@ -1,9 +1,14 @@
 import { generateCommitMessage } from './openai/generate-commit-message';
-
+import { Octokit } from '@octokit/rest';
 import { call } from './core';
 
-call(async ({ github, context }) => {
-  const { data: diff } = await github.rest.pulls.get({
+call(async ({ github, context, core }) => {
+  const token = core.getInput('github-token', { required: true });
+  const octo = new Octokit({
+    auth: token,
+  });
+  // 不知道为什么，@actions/github 自带的那个 octo 请求不到 patch 类型
+  const { data: diff } = await octo.pulls.get({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: context.issue.number,
@@ -11,6 +16,10 @@ call(async ({ github, context }) => {
       format: 'diff',
     },
   });
+
+  console.log(`🚀 ~ file: codeReview.ts:14 ~ call ~ diff:`, diff);
+  console.log(typeof diff);
+
   const msg = await generateCommitMessage(diff as unknown as string);
   await github.rest.issues.createComment({
     issue_number: context.issue.number,
