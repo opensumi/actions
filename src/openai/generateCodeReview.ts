4e9e7b20b9a6111e56d5284e4e70eeefaf073bb4
野声 <lijiacheng.ljc@antgroup.com>
2023-03-10 16:27:41 +0800

    chore: adjust prompt

--- src/openai/generateCodeReview.ts
+++ src/openai/generateCodeReview.ts
@@ -12,7 +12,7 @@ export async function generateCodeReview(title: string, content: string) {
     messages: [
       {
         role: 'system',
-        content: `用户输入的是某次pr的diff文件，该PR的名字为：${title}。你有两个任务：1.进行 CodeReview，检查代码中是否有明显的错误，typo 等信息。2. 为该次 PR 生成合适的 PR title 和 commit message，PR title 和 commit message 应遵循 Angular 规范。\n\n你的回复要尽可能的精简，准确；不要输出除了答案之外多余的内容。`,
+        content: `用户输入的是某次 pr 的diff文件，该PR的名字为：${title}。你有两个任务：1.进行 CodeReview，检查代码中是否有明显的错误，typo 等信息。2. 为该次 PR 生成合适的 PR title 和 commit message，PR title 和 commit message 应遵循 Angular 规范。`,
       },
       {
         role: 'user',
