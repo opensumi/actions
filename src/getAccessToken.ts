2485619a7451c50e58d3aa8e7f34bb24e78b05e2
野声 <lijiacheng.ljc@antgroup.com>
2023-03-30 11:06:11 +0800

    fix: get bot token action

--- src/getAccessToken.ts
+++ src/getAccessToken.ts
@@ -1,20 +1,25 @@
 import * as core from '@actions/core';
+import undici from 'undici';
 
 async function main() {
-  const tokenServer = core.getInput('token-server');
-  const flag = core.getInput('flag');
+  try {
+    const tokenServer = core.getInput('token-server');
+    const flag = core.getInput('flag');
 
-  const token = await fetch(tokenServer, {
-    headers: {
-      Authorization: `flag ${flag}`,
-    },
-  });
+    const token = await undici.request(tokenServer, {
+      headers: {
+        Authorization: `flag ${flag}`,
+      },
+    });
 
-  const tokenStr = (await token.json()).token;
+    const tokenStr = (await token.body.json()).token;
 
-  core.exportVariable('GITHUB_TOKEN', tokenStr);
-  core.setOutput('token', tokenStr);
-  core.setSecret(tokenStr);
+    core.exportVariable('GITHUB_TOKEN', tokenStr);
+    core.setOutput('token', tokenStr);
+    core.setSecret(tokenStr);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 main();
