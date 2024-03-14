ad9c76ba921c19bcbab4bf512a49f2de27514f74
野声 <lijiacheng.ljc@antgroup.com>
2024-03-14 11:23:35 +0800

    feat: update version notice

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,7 +4,7 @@ import { Context } from './core';
 export const createVersionText = (type = 'Pre-Release', version: string) => {
   return (
     `<!-- versionInfo: ${type} | ${version} -->\n` +
-    `🎉 ${type} version ${version}  publish successful! You can install this version via \`npm install package@${version}\`\n\n` +
+    `🎉 ${type} version ${version}  publish successful!\n\n` +
     '```\n' +
     version +
     '\n' +
