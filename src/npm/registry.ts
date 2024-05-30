d314135bc1e414eb1a63e7b9345721d9b7148a64
野声 <lijiacheng.ljc@antgroup.com>
2024-05-30 18:42:53 +0800

    feat: sync codeblitz

--- src/npm/registry.ts
+++ src/npm/registry.ts
@@ -38,7 +38,7 @@ export class Registry {
       method: 'PUT',
     });
     if (resp.statusCode === 201) {
-      const data = await resp.body.json();
+      const data = (await resp.body.json()) as any;
       console.log(`get ${pkgName} log id`, data);
       if (data.ok) {
         const logId = data.logId as string;
@@ -55,7 +55,7 @@ export class Registry {
     pkgName: string,
     logId: string,
     offset: number = 0,
-    waitForFirstLog = 0
+    waitForFirstLog = 0,
   ) {
     if (waitForFirstLog > 50) {
       console.log('waitForFirstLog times > 50, return');
@@ -68,7 +68,7 @@ export class Registry {
         offset,
       },
     });
-    const data = await resp.body.json();
+    const data = (await resp.body.json()) as any;
     if (data.log) {
       const log = (data.log as string).trim();
       console.log('log:', log);
@@ -81,7 +81,7 @@ export class Registry {
         pkgName,
         logId,
         offset + lines,
-        waitForFirstLog
+        waitForFirstLog,
       );
     }
     await sleep(500);
@@ -98,3 +98,64 @@ export class Registry {
 export const cnpmRegistry = new Registry('https://r.cnpmjs.org');
 export const npmmirrorRegistry = new Registry('https://registry.npmmirror.com');
 export const npmRegistry = new Registry('https://registry.npmjs.com');
+
+export async function sync(pkgs: string[]) {
+  const toPromise = [] as any[];
+  for (const p of pkgs) {
+    toPromise.push(
+      (async () => {
+        console.log(`sync ${p}`);
+        await cnpmRegistry.sync(p);
+        await npmmirrorRegistry.sync(p);
+        console.log(`sync ${p} done`);
+      })(),
+    );
+  }
+
+  await Promise.all(toPromise);
+}
+
+export const syncOnePackage = async (
+  registry: Registry,
+  p: string,
+  version: string,
+  retryTimes = 0,
+) => {
+  console.log(`start sync ${p}, retryTimes: ${retryTimes}`);
+  if (await registry.versionExists(p, version)) {
+    console.log(`${p}@${version} exists`);
+    return true;
+  }
+
+  if (retryTimes > 20) {
+    console.log('stop retry package:', p, 'retryTimes:', retryTimes);
+    return false;
+  }
+
+  await registry.sync(p);
+
+  console.log(`sync ${p} done`);
+
+  return await syncOnePackage(registry, p, version, retryTimes + 1);
+};
+
+export async function syncOneVersion(pkgs: string[], version: string) {
+  const toPromise = [] as Promise<void>[];
+  for (const p of pkgs) {
+    toPromise.push(
+      (async () => {
+        // 只有当 npm registry 上有这个包再同步。
+        if (await npmRegistry.versionExists(p, version)) {
+          const result = await syncOnePackage(cnpmRegistry, p, version);
+          console.log(`=== sync ${p} cnpm result`, result);
+          const result2 = await syncOnePackage(npmmirrorRegistry, p, version);
+          console.log(`=== sync ${p} npmmirror result`, result2);
+        } else {
+          console.error(`=== ${p} ${version} 在 npm 上不存在，请检查。`);
+        }
+      })(),
+    );
+  }
+
+  await Promise.all(toPromise);
+}
--- src/sync.ts
+++ src/sync.ts
@@ -1,69 +1,11 @@
 import { opensumiPackages } from './npm/constants';
-import {
-  cnpmRegistry,
-  Registry,
-  npmRegistry,
-  npmmirrorRegistry,
-} from './npm/registry';
+import { sync, syncOneVersion } from './npm/registry';
 
 const version = process.argv.slice(2)[0];
 
 if (!version) {
-  (async () => {
-    const toPromise = [] as any[];
-    for (const p of opensumiPackages) {
-      toPromise.push(
-        (async () => {
-          console.log(`sync ${p}`);
-          await cnpmRegistry.sync(p);
-          await npmmirrorRegistry.sync(p);
-          console.log(`sync ${p} done`);
-        })(),
-      );
-    }
-
-    await Promise.all(toPromise);
-  })();
+  sync(opensumiPackages);
 } else {
   console.log(`start poll version`, version);
-
-  const syncOnePackage = async (registry: Registry, p, retryTimes = 0) => {
-    console.log(`start sync ${p}, retryTimes: ${retryTimes}`);
-    if (await registry.versionExists(p, version)) {
-      console.log(`${p}@${version} exists`);
-      return true;
-    }
-
-    if (retryTimes > 20) {
-      console.log('stop retry package:', p, 'retryTimes:', retryTimes);
-      return false;
-    }
-
-    await registry.sync(p);
-
-    console.log(`sync ${p} done`);
-
-    return await syncOnePackage(registry, p, retryTimes + 1);
-  };
-
-  (async () => {
-    const toPromise = [] as Promise<void>[];
-    for (const p of opensumiPackages) {
-      toPromise.push(
-        (async () => {
-          // 只有当 npm registry 上有这个包再同步。
-          if (await npmRegistry.versionExists(p, version)) {
-            const result = await syncOnePackage(cnpmRegistry, p);
-            console.log(`=== sync ${p} cnpm result`, result);
-            const result2 = await syncOnePackage(npmmirrorRegistry, p);
-            console.log(`=== sync ${p} npmmirror result`, result2);
-          } else {
-            console.error(`=== ${p} ${version} 在 npm 上不存在，请检查。`);
-          }
-        })(),
-      );
-    }
-
-    await Promise.all(toPromise);
-  })();
+  syncOneVersion(opensumiPackages, version);
 }
