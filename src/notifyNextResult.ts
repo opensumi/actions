7f15f30143750ea78f1979d6e330b05bcd60e67f
野声 <lijiacheng.ljc@antgroup.com>
2024-06-06 12:03:30 +0800

    feat(codeblitz): support release next by ref

--- src/notifyNextResult.ts
+++ src/notifyNextResult.ts
@@ -1,12 +1,14 @@
 import { call } from './core';
 import { createVersionText } from './helpers';
+import { getRepo } from './utils/context';
 
 call(async ({ github, context }) => {
   const commentBody = createVersionText('Next', process.env.CURRENT_VERSION!);
 
+  const repo = getRepo(context);
+
   await github.rest.repos.createCommitComment({
-    owner: 'opensumi',
-    repo: 'core',
+    ...repo,
     body: commentBody,
     commit_sha: process.env.CURRENT_COMMIT,
   });
