dab0da9b3a52c18f7a1c77ab4fae5847ea7ac309
野声 <lijiacheng.ljc@antgroup.com>
2024-07-30 18:52:32 +0800

    fix: lerna publish next

--- src/lerna-publish/index.ts
+++ src/lerna-publish/index.ts
@@ -2,11 +2,38 @@ import { call } from '../core';
 import dayjs from 'dayjs';
 import { execAsync } from '../utils/exec';
 
+function handleSha(sha: string) {
+  if (!sha) {
+    return 'next';
+  }
+  return sha.replace(/[/\\-]/g, ' ').slice(0, 6);
+}
+
+function handleBranch(branch: string) {
+  if (!branch) {
+    return 'next';
+  }
+
+  const tmp = branch.replace(/[/\\-]/g, ' ');
+
+  if (tmp.length > 6) {
+    // 前三个和后三个
+    return `${tmp.slice(0, 3)}${tmp.slice(-3)}`;
+  }
+
+  return tmp;
+}
+
 call(async ({ github, context, core, meta }) => {
   const dateString = dayjs().format('YYYYMMDDHHmmss');
-  const headSha = process.env.HEAD_SHA!.slice(0, 6);
+  let sha = '';
+  if (process.env.SHA) {
+    sha = handleSha(process.env.SHA);
+  } else {
+    sha = handleBranch(process.env.GITHUB_REF);
+  }
 
-  const version = `0.0.${dateString}-${headSha}.0`;
+  const version = `0.0.${dateString}-${sha}.0`;
 
   core.info(`Publishing ${version}`);
 
